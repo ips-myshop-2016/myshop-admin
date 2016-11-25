78fc1ada65b5f1b2be2586943a4fac718dc66cdd
Palaskan <onecarlostlol@gmail.com>
2016-11-25 12:44:23 +0100

    Primero, tercero y cuarto informes realizados, resta el segundo
    Signed-off-by: Palaskan <onecarlostlol@gmail.com>

--- Myshop - Admin Module/src/main/java/com/myshop/admin/controller/ControllerInformes.java
+++ Myshop - Admin Module/src/main/java/com/myshop/admin/controller/ControllerInformes.java
@@ -11,15 +11,33 @@ public class ControllerInformes {
 
 	
 	public List<Map<String,Object>> primerInforme() {
-		//TODO
-		String complexSql = "" ;
+		String complexSql = "SELECT Fecha, ParticularesCON, ParticularesSIN, "
+				+ "Minoristas FROM ((SELECT parti.Date AS Fecha,IFNULL(parti.particularesSIN, 0) "
+				+ "AS ParticularesSIN, IFNULL(parti.particularesCON, 0) AS ParticularesCON, IFNULL"
+				+ "(mino.minoristas, 0) AS Minoristas FROM (SELECT DATE(date_received) AS Date, COUNT"
+				+ "(CASE WHEN C.user_id IS NULL THEN 1 ELSE NULL END) AS particularesSIN, "
+				+ "COUNT(CASE WHEN C.user_id IS NOT NULL THEN 1 ELSE NULL END) AS particularesCON "
+				+ "FROM myshop.order O, myshop.individual_customer C WHERE O.customer_id = C.customer_id "
+				+ "GROUP BY DATE(date_received)) AS parti LEFT JOIN (SELECT DATE(O.date_received) AS Date, "
+				+ "COUNT(*) AS minoristas FROM myshop.order O, myshop.company CO WHERE O.customer_id = CO.company_id"
+				+ " GROUP BY DATE(O.date_received)) AS mino ON parti.Date = mino.Date) "
+				+ "UNION (SELECT mino.Date AS Fecha, IFNULL(parti.particularesSIN, 0) AS"
+				+ " ParticularesSIN, IFNULL(parti.particularesCON, 0) AS ParticularesCON, "
+				+ "IFNULL(mino.minoristas, 0) AS Minoristas FROM (SELECT DATE(date_received) "
+				+ "AS Date, COUNT(CASE WHEN C.user_id IS NULL THEN 1 ELSE NULL END) AS "
+				+ "particularesSIN, COUNT(CASE WHEN C.user_id IS NOT NULL THEN 1 ELSE NULL END) "
+				+ "AS particularesCON FROM myshop.order O, myshop.individual_customer C "
+				+ "WHERE O.customer_id = C.customer_id GROUP BY DATE(date_received)) AS "
+				+ "parti RIGHT JOIN (SELECT DATE(O.date_received) AS Date, COUNT(*) AS "
+				+ "minoristas FROM myshop.order O, myshop.company CO WHERE  "
+				+ "O.customer_id = CO.company_id GROUP BY DATE(O.date_received)) AS mino"
+				+ " ON parti.Date = mino.Date)) AS resultado ORDER BY Fecha";
 		try (Connection con = DefaultSql2o.SQL2O.open()) {
 			return con.createQuery(complexSql).executeAndFetchTable().asList();
 		}
 	}
 	
 	public List<Map<String,Object>> segundoInforme() {
-		//TODO
 		String complexSql = "";
 		try (Connection con = DefaultSql2o.SQL2O.open()) {
 			return con.createQuery(complexSql).executeAndFetchTable().asList();
--- Myshop - Admin Module/src/main/java/com/myshop/admin/ui/VentanaPrincipal.java
+++ Myshop - Admin Module/src/main/java/com/myshop/admin/ui/VentanaPrincipal.java
@@ -26,6 +26,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -166,7 +168,11 @@ public class VentanaPrincipal extends JFrame {
 			lblTercerInforme.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent arg0) {
-					rellenarTercerInforme();
+					try {
+						rellenarTercerInforme();
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
 					getPnPrimerInforme().setVisible(false);
 					getPnSegundoInforme().setVisible(false);
 					getPnCuartoInforme().setVisible(false);
@@ -182,7 +188,11 @@ public class VentanaPrincipal extends JFrame {
 			lblCuartoInforme.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent arg0) {
-					rellenarCuartoInforme();
+					try {
+						rellenarCuartoInforme();
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
 					getPnPrimerInforme().setVisible(false);
 					getPnSegundoInforme().setVisible(false);
 					getPnTercerInforme().setVisible(false);
@@ -233,7 +243,7 @@ public class VentanaPrincipal extends JFrame {
 	}
 	private JTable getTbTercerInforme() {
 		if (tbTercerInforme == null) {
-			String[] columnas = { "Dia","Empaquetados almacenero 1", "Empaquetados almacenero 2"};
+			String[] columnas = { "Dia","Empaquetados a1", "Empaquetados a2","Empaquetados a3", "Empaquetados a4","Empaquetados a5"};
 			modeloTablaTercerInforme = new DefaultTableModel(columnas, 0) {
 				private static final long serialVersionUID = 1L;
 
@@ -254,10 +264,10 @@ public class VentanaPrincipal extends JFrame {
 		List<Map<String,Object>> informe = new ControllerInformes().primerInforme();
 		
 		for (Map<String,Object> map : informe) {
-			/*nuevaFila[0] = map.get("date");
-			nuevaFila[1] = map.get("suma1");
-			nuevaFila[2] = map.get("suma2");
-			nuevaFila[3] = map.get("suma3")*/
+			nuevaFila[0] = map.get("fecha");
+			nuevaFila[1] = map.get("particularessin");
+			nuevaFila[2] = map.get("particularescon");
+			nuevaFila[3] = map.get("minoristas");
 			modeloTablaPrimerInforme.addRow(nuevaFila);
 		}
 	}
@@ -278,7 +288,7 @@ public class VentanaPrincipal extends JFrame {
 		}
 	}
 
-	private boolean estaFecha(Date d,List<Map<String,Object>> l){
+	private boolean estaFecha(String d,List<Map<String,Object>> l){
 		for(Map<String,Object> m : l){
 			if(m.get("date").equals(d)){
 				return true;
@@ -287,15 +297,24 @@ public class VentanaPrincipal extends JFrame {
 		return false;
 	}
 	
-	private void rellenarTercerInforme(){
+	private void rellenarTercerInforme() throws ParseException{
 		reiniciarTercerInforme();
-		Object[] nuevaFila = new Object[3];
+		Object[] nuevaFila = new Object[6];
 		List<Map<String,Object>> informe = new ControllerInformes().tercerInforme();
-		
+		List<Map<String,Object>> aux = new ArrayList<Map<String,Object>>();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Map<String,Object> mp;
+		for(Map<String,Object> m : informe){
+			mp = new HashMap<String,Object>();
+			mp.put("wk_id", m.get("wk_id"));
+			mp.put("date_received",sdf.format((Date)m.get("date_received")));
+			mp.put("suma", m.get("suma"));
+			aux.add(mp);
+		}
 		List<Map<String,Object>> res = new ArrayList<Map<String,Object>>();
-		for (Map<String,Object> map1 : informe) {
+		for (Map<String,Object> map1 : aux) {
 			Map<String,Object> mapa = new HashMap<String,Object>();
-			if(!estaFecha((Date)map1.get("date_received"),res)){
+			if(!estaFecha((String)map1.get("date_received"),res)){
 				mapa.put("date", map1.get("date_received"));
 				mapa.put("suma1", map1.get("suma"));
 				mapa.put("id", map1.get("wk_id"));
@@ -303,7 +322,7 @@ public class VentanaPrincipal extends JFrame {
 			}
 		}
 		
-		for (Map<String,Object> map1 : informe) {
+		for (Map<String,Object> map1 : aux) {
 			for(Map<String,Object> mapa2 : res){
 				if(map1.get("wk_id") != null && !map1.get("wk_id").equals(mapa2.get("id")) 
 						&& map1.get("date_received").equals(mapa2.get("date"))){
@@ -322,21 +341,41 @@ public class VentanaPrincipal extends JFrame {
 				nuevaFila[2] = 0;
 			else
 				nuevaFila[2] = map.get("suma2");
+			if(map.get("suma3") == null)
+				nuevaFila[3] = 0;
+			else
+				nuevaFila[3] = map.get("suma3");
+			if(map.get("suma4") == null)
+				nuevaFila[4] = 0;
+			else
+				nuevaFila[4] = map.get("suma4");
+			if(map.get("suma5") == null)
+				nuevaFila[5] = 0;
+			else
+				nuevaFila[5] = map.get("suma5");
 			modeloTablaTercerInforme.addRow(nuevaFila);
 			
 		}
 	}
 	
-	private void rellenarCuartoInforme(){
+	private void rellenarCuartoInforme() throws ParseException{
 		reiniciarCuartoInforme();
-		
-		Object[] nuevaFila = new Object[3];
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");      
+		Object[] nuevaFila = new Object[6];
 		List<Map<String,Object>> informe = new ControllerInformes().cuartoInforme();
-
+		List<Map<String,Object>> aux = new ArrayList<Map<String,Object>>();
+		Map<String,Object> mp;
+		for(Map<String,Object> m : informe){
+			mp = new HashMap<String,Object>();
+			mp.put("wk_id", m.get("wk_id"));
+			mp.put("date_completed",sdf.format((Date)m.get("date_completed")));
+			mp.put("suma", m.get("suma"));
+			aux.add(mp);
+		}
 		List<Map<String,Object>> res = new ArrayList<Map<String,Object>>();
-		for (Map<String,Object> map1 : informe) {
+		for (Map<String,Object> map1 : aux) {
 			Map<String,Object> mapa = new HashMap<String,Object>();
-			if(!estaFecha((Date)map1.get("date_completed"),res)){
+			if(!estaFecha((String)map1.get("date_completed"),res)){
 				mapa.put("date", map1.get("date_completed"));	
 				if((int)map1.get("wk_id") == 1){
 					mapa.put("suma1", map1.get("suma"));
@@ -346,18 +385,37 @@ public class VentanaPrincipal extends JFrame {
 					mapa.put("suma2", map1.get("suma"));
 					mapa.put("id", map1.get("wk_id"));
 				}
+				if((int)map1.get("wk_id") == 3){
+					mapa.put("suma3", map1.get("suma"));
+					mapa.put("id", map1.get("wk_id"));
+				}
+				if((int)map1.get("wk_id") == 4){
+					mapa.put("suma4", map1.get("suma"));
+					mapa.put("id", map1.get("wk_id"));
+				}
+				if((int)map1.get("wk_id") == 5){
+					mapa.put("suma5", map1.get("suma"));
+					mapa.put("id", map1.get("wk_id"));
+				}
 				res.add(mapa);
 			}
 		}
 		
-		for (Map<String,Object> map1 : informe) {
+		for (Map<String,Object> map1 : aux) {
 			for(Map<String,Object> mapa2 : res){
 				if(map1.get("wk_id") != null && !map1.get("wk_id").equals(mapa2.get("id")) 
 						&& map1.get("date_completed").equals(mapa2.get("date"))){
-					if((int)mapa2.get("id") == 2)
-						mapa2.put("suma2", map1.get("suma"));
 					if((int)mapa2.get("id") == 1)
 						mapa2.put("suma1", map1.get("suma"));
+					if((int)mapa2.get("id") == 2)
+						mapa2.put("suma2", map1.get("suma"));
+					if((int)mapa2.get("id") == 3)
+						mapa2.put("suma3", map1.get("suma"));
+					if((int)mapa2.get("id") == 4)
+						mapa2.put("suma4", map1.get("suma"));
+					if((int)mapa2.get("id") == 5)
+						mapa2.put("suma5", map1.get("suma"));
+
 				}
 			}
 		}
@@ -372,6 +430,18 @@ public class VentanaPrincipal extends JFrame {
 				nuevaFila[2] = 0;
 			else
 				nuevaFila[2] = map.get("suma2");
+			if(map.get("suma3") == null)
+				nuevaFila[3] = 0;
+			else
+				nuevaFila[3] = map.get("suma3");
+			if(map.get("suma4") == null)
+				nuevaFila[4] = 0;
+			else
+				nuevaFila[4] = map.get("suma4");
+			if(map.get("suma5") == null)
+				nuevaFila[5] = 0;
+			else
+				nuevaFila[5] = map.get("suma5");
 			modeloTablaCuartoInforme.addRow(nuevaFila);
 			
 		}
@@ -409,7 +479,7 @@ public class VentanaPrincipal extends JFrame {
 	}
 	private JTable getTbPrimerInforme() {
 		if (tbPrimerInforme == null) {
-			String[] columnas = {  "Dia","N.Compras U.Anonimo", "N.Compras U.Registrado","N.Compras Empresas"};
+			String[] columnas = {  "Dia","N.Compras U.Anonimo", "N.Compras U.Registrado","N.Compras Minoristas"};
 			modeloTablaPrimerInforme = new DefaultTableModel(columnas, 0) {
 				private static final long serialVersionUID = 1L;
 
@@ -451,7 +521,7 @@ public class VentanaPrincipal extends JFrame {
 	}
 	private JTable getTbCuartoInforme() {
 		if (tbCuartoInforme == null) {
-			String[] columnas = {  "Dia","OrdenesTrabajo almacenero 1", "OrdenesTrabajo almacenero 2"};
+			String[] columnas = {  "Dia","OrdenesTrabajo a1", "OrdenesTrabajo a2","OrdenesTrabajo a3", "OrdenesTrabajo a4","OrdenesTrabajo a5"};
 			modeloTablaCuartoInforme = new DefaultTableModel(columnas, 0) {
 				private static final long serialVersionUID = 1L;
 
